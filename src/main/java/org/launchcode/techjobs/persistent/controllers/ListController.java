5da16932495b3692322b890ed88e146bf55b4ea1
ShadowyBlue <alandchung09@gmail.com>
2024-06-12 17:03:36 -0500

    passes unit tests

--- src/main/java/org/launchcode/techjobs/persistent/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/ListController.java
@@ -1,8 +1,11 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.launchcode.techjobs.persistent.models.JobData;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -20,7 +24,73 @@ import java.util.HashMap;
 public class ListController {
 
     @Autowired
-    private JobRepository jobRepository;
+    private JobRepository jobRepository = new JobRepository() {
+        @Override
+        public <S extends Job> S save(S entity) {
+            return null;
+        }
+
+        @Override
+        public <S extends Job> Iterable<S> saveAll(Iterable<S> entities) {
+            return null;
+        }
+
+        @Override
+        public Optional<Job> findById(Integer integer) {
+            return Optional.empty();
+        }
+
+        @Override
+        public boolean existsById(Integer integer) {
+            return false;
+        }
+
+        @Override
+        public Iterable<Job> findAll() {
+            return null;
+        }
+
+        @Override
+        public Iterable<Job> findAllById(Iterable<Integer> integers) {
+            return null;
+        }
+
+        @Override
+        public long count() {
+            return 0;
+        }
+
+        @Override
+        public void deleteById(Integer integer) {
+
+        }
+
+        @Override
+        public void delete(Job entity) {
+
+        }
+
+        @Override
+        public void deleteAllById(Iterable<? extends Integer> integers) {
+
+        }
+
+        @Override
+        public void deleteAll(Iterable<? extends Job> entities) {
+
+        }
+
+        @Override
+        public void deleteAll() {
+
+        }
+    };
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
 
     static HashMap<String, String> columnChoices = new HashMap<>();
 
@@ -34,7 +104,9 @@ public class ListController {
 
     @RequestMapping("")
     public String list(Model model) {
-
+        model.addAttribute("jobs", jobRepository.findAll());
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills",skillRepository.findAll());
         return "list";
     }
 
